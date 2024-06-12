bddd3ad73399d75ac8055f026bb7fcdc823ae899
vivek <chimnanivivek14@gmail.com>
2024-06-12 12:03:27 +0530

    Added an image and button in the main screen

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -17,6 +17,36 @@ class GradientContainer extends StatelessWidget {
           end: end,
         ),
       ),
+      child: Center(
+          child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            "assets/images/quiz-logo.png",
+            width: 300,
+          ),
+          const Padding(
+            padding: EdgeInsets.only(
+              top: 50,
+              bottom: 50,
+            ),
+            child: Text(
+              "Learn Flutter the fun way!",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text("Start Quiz"),
+            style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.transparent),
+          ),
+        ],
+      )),
     );
   }
 }
