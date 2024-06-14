3601ee1a3d7fefe0f36895157994e3ee4d13288b
vivek <chimnanivivek14@gmail.com>
2024-06-14 12:23:37 +0530

    styled the results screen

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:third_app/question_identifier.dart';
+import 'package:third_app/summary_item.dart';
 
 class QuestionsSummary extends StatelessWidget {
   const QuestionsSummary(this.summaryData, {super.key});
@@ -8,27 +10,15 @@ class QuestionsSummary extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 300,
+      height: 400,
       child: SingleChildScrollView(
         child: Column(
-            children: summaryData.map((data) {
-          return Row(
-            children: [
-              Text(
-                ((data['question_index'] as int) + 1).toString(),
-              ),
-              Expanded(
-                child: Column(
-                  children: [
-                    Text(data["question"] as String),
-                    Text(data["correct_answer"] as String),
-                    Text(data["user_answer"] as String),
-                  ],
-                ),
-              )
-            ],
-          );
-        }).toList()),
+          children: summaryData.map(
+            (data) {
+              return SummaryItem(data);
+            },
+          ).toList(),
+        ),
       ),
     );
   }
